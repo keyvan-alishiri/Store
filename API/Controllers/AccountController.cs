b9965096a2813a6007ad8ef18a9ec98fa05b828f
keyvan-alishiri <keyvan.alishiri.com>
2021-09-11 03:26:03 +0430

    End Of section 16

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -104,6 +104,10 @@ namespace API.Controllers
         [HttpPost("register")]
         public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
+            if(CheckEmailExistsAsync(registerDto.Email).Result.Value)
+            {
+                return new  BadRequestObjectResult(new ApiValidationErrorResponse {Errors = new []{"Email Address is in use"}});
+            }
             var user = new AppUser
             {
                 DispalyName = registerDto.DisplayName,
--- API/Controllers/BasketController.cs
+++ API/Controllers/BasketController.cs
@@ -1,5 +1,8 @@
+using System.Globalization;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
+using API.Dtos;
+using AutoMapper;
 using Core.Entities;
 using Core.Interfaces;
 using Infrastructure;
@@ -10,8 +13,10 @@ namespace API.Controllers
     public class BasketController : BaseApiController
     {
         private readonly IBasketRepository _basketRepository;
-        public BasketController(IBasketRepository basketRepository)
+        private readonly IMapper _mapper;
+        public BasketController(IBasketRepository basketRepository, IMapper mapper)
         {
+            _mapper = mapper;
             _basketRepository = basketRepository;
         }
 
@@ -23,9 +28,10 @@ namespace API.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult<CustomerBasket>> UpdateBasket(CustomerBasket basket)
+        public async Task<ActionResult<CustomerBasket>> UpdateBasket(CustomerBasketDto basket)
         {
-            var updatedBasket = await _basketRepository.UpdateBasketAsync(basket);
+            var customerBasket = _mapper.Map<CustomerBasketDto, CustomerBasket>(basket);
+            var updatedBasket = await _basketRepository.UpdateBasketAsync(customerBasket);
             return Ok(updatedBasket);
         }
         [HttpDelete]
